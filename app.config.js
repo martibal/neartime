4c692502a80698a8968da103517d21f16f507532
martibal <69890184+martibal@users.noreply.github.com>
2026-09-14 00:20:32 +0200

    fix: configure Google Maps through react-native-maps plugin

--- app.config.js
+++ app.config.js
@@ -1,20 +1,18 @@
 module.exports = ({ config }) => {
   const googleMapsApiKey = process.env.GOOGLE_MAPS_ANDROID_API_KEY;
+  const plugins = [...(config.plugins ?? [])];
+
+  if (googleMapsApiKey) {
+    plugins.push([
+      'react-native-maps',
+      {
+        androidGoogleMapsApiKey: googleMapsApiKey,
+      },
+    ]);
+  }
 
   return {
     ...config,
-    android: {
-      ...config.android,
-      ...(googleMapsApiKey
-        ? {
-            config: {
-              ...(config.android?.config ?? {}),
-              googleMaps: {
-                apiKey: googleMapsApiKey,
-              },
-            },
-          }
-        : {}),
-    },
+    plugins,
   };
 };
